5429511a7491625bb4ae6f6d8b94ef5adcda88ed
Karthik Narayanan <karthikn@ufl.edu>
2017-01-25 03:39:15 -0500

    Add files via upload

---
+++ Bishop.java
@@ -0,0 +1,43 @@
+//Problem        : Bishop
+//Language       : Java
+//Compiled Using : javac
+//Version        : Java 1.7.0_75
+//Input for your program will be provided from STDIN
+//Print out all output from your program to STDOUT
+
+import java.util.Scanner;
+
+public class Bishop
+{
+    public static void main(String[] args)
+    {
+
+        Scanner stdin = new Scanner(System.in);
+        int sB = stdin.nextInt();
+        int eB = stdin.nextInt();
+        int moves = 0;
+        int sX, sY, eX, eY;
+        
+        sX = sB / 8;
+        if( sB % 8 == 0 )
+            sX--;
+        eX = eB / 8;
+        if( eB % 8 == 0 )
+            eX--;
+        sY = ( sB - 1 ) % 8;
+        eY = ( eB - 1 ) % 8;
+        
+        if( sB == eB )
+            moves = 0;
+        else if ( ( ( ( sX + sY ) % 2 == 0 ) && ( ( eX + eY ) % 2 != 0 ) ) || ( ( ( sX + sY ) % 2 != 0 ) && ( ( eX + eY ) % 2 == 0 ) ) )
+            moves = -1;
+        else if ( ( sX + sY == eX + eY ) || ( sX - sY == eX - eY ) )
+            moves = 1;
+        else
+            moves = 2;
+
+        System.out.println(moves);
+        stdin.close();
+    }
+
+}
